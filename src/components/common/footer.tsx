d82d3066984c7f5b7f3de8473bb3007b4df1e45d
asdflkj169 <asdflkj169@gmail.com>
2026-01-17 12:25:32 +0000

    refine

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,18 +1,22 @@
-import { ROUTES_MAP } from "@/router";
 import { Link } from "react-router";
+import { PATHS } from "@/router";
 
 function Footer() {
+  const linkItems = Object.values(PATHS)
+    .filter((path) => path.label !== "Test2_3")
+    .map((path) => (
+      <Link
+        key={path.label}
+        to={path.path}
+        className="font-medium text-sm hover:text-gray-200 hover:underline"
+      >
+        {path?.label}
+      </Link>
+    ));
   return (
     <footer className="bg-gray-600 p-4 text-white">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 md:flex-row">
-        <nav className="flex gap-6">
-          <Link
-            to={ROUTES_MAP.Home}
-            className="font-medium text-sm hover:text-gray-200 hover:underline"
-          >
-            Home
-          </Link>
-        </nav>
+        <nav className="flex gap-6">{linkItems}</nav>
         <div className="text-gray-300 text-sm">
           © 2026 Astra Mendacium. All rights reserved.
         </div>
--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,4 @@
-import {
-  createBrowserRouter,
-  type IndexRouteObject,
-  type NonIndexRouteObject,
-  type RouteObject,
-} from "react-router";
+import { createBrowserRouter, type RouteObject } from "react-router";
 import App from "@/App";
 import Home from "@/pages/home";
 import Test1 from "@/pages/test1";
@@ -11,70 +6,42 @@ import Test2 from "@/pages/test2";
 import Test2_1 from "@/pages/test2_1";
 import Test2_2 from "@/pages/test2_2";
 import Test2_3 from "@/pages/test2_3";
-import { never } from "valibot";
 
-export const ROUTES_ID = {
-  HOME: "Home",
-  TEST1: "Test1",
-  TEST2_1: "Test2_1",
-  TEST2_2: "Test2_2",
-  TEST2_3: "Test2_3",
+export const SEGMENTS = {
+  home: "",
+  test1: "test1",
+  test2: "test2",
+  test2_1: "test2_1",
+  test2_2: "test2_2",
+  test2_3: ":id",
 } as const;
 
-type RoutesId = (typeof ROUTES_ID)[keyof typeof ROUTES_ID];
-
-interface CustomIndexRouteObject extends IndexRouteObject {
-  routesId: RoutesId;
-}
-
-interface CustomNonIndexRouteObjectWithId
-  extends Omit<NonIndexRouteObject, "children"> {
-  routesId: RoutesId;
-  children?: undefined;
-}
-
-interface CustomNonIndexRouteObjectWithChildren
-  extends Omit<NonIndexRouteObject, "id" | "children"> {
-  routesId?: undefined;
-  children: CustomRouteObject[];
-}
-
-type CustomRouteObject =
-  | CustomIndexRouteObject
-  | CustomNonIndexRouteObjectWithId
-  | CustomNonIndexRouteObjectWithChildren;
-
-export const ROUTES: CustomRouteObject = {
-  path: "",
+export const ROUTES: RouteObject = {
+  path: "/",
   Component: App,
   children: [
     {
-      routesId: ROUTES_ID.HOME,
-      path: "",
+      path: SEGMENTS.home,
       Component: Home,
     },
     {
-      routesId: ROUTES_ID.TEST1,
-      path: "test1",
+      path: SEGMENTS.test1,
       Component: Test1,
     },
     {
-      path: "test2",
+      path: SEGMENTS.test2,
       Component: Test2,
       children: [
         {
-          routesId: ROUTES_ID.TEST2_1,
-          path: "test2_1",
+          path: SEGMENTS.test2_1,
           Component: Test2_1,
         },
         {
-          routesId: ROUTES_ID.TEST2_2,
-          path: "test2_2",
+          path: SEGMENTS.test2_2,
           Component: Test2_2,
         },
         {
-          routesId: ROUTES_ID.TEST2_3,
-          path: ":id",
+          path: SEGMENTS.test2_3,
           Component: Test2_3,
         },
       ],
@@ -82,40 +49,27 @@ export const ROUTES: CustomRouteObject = {
   ],
 };
 
-function toMap(route: CustomRouteObject): Map<RoutesId, string[]> {
-  const id = route.routesId;
-  const path = route.path;
-  const children = route.children;
-  if (id && path !== undefined) {
-    return new Map([[id, [path]]]);
-  }
-
-  if (children) {
-    const childMaps = children.map(toMap);
-    const merged = new Map<RoutesId, string[]>();
-    for (const childMap of childMaps) {
-      for (const [childId, childPathSegments] of childMap.entries()) {
-        merged.set(
-          childId,
-          path ? [path, ...childPathSegments] : childPathSegments,
-        );
-      }
-    }
-    return merged;
-  }
-  throw new Error(
-    `Route must have either an id and path, or children: ${JSON.stringify(route)}`,
-  );
-}
-
-export const ROUTES_MAP: Record<RoutesId, string> = toMap(ROUTES)
-  .entries()
-  .reduce(
-    (acc, [key, value]) => {
-      acc[key] = value.join("/");
-      return acc;
-    },
-    {} as Record<RoutesId, string>,
-  );
+export const PATHS = {
+  home: {
+    label: "Home",
+    path: "/",
+  },
+  test1: {
+    label: "Test1",
+    path: `/${SEGMENTS.test1}`,
+  },
+  test2_1: {
+    label: "Test2_1",
+    path: `/${SEGMENTS.test2}/${SEGMENTS.test2_1}`,
+  },
+  test2_2: {
+    label: "Test2_2",
+    path: `/${SEGMENTS.test2}/${SEGMENTS.test2_2}`,
+  },
+  test2_3: {
+    label: "Test2_3",
+    pathWithParam: (id: string) => `/${SEGMENTS.test2}/${id}`,
+  },
+} as const;
 
 export const router = createBrowserRouter([ROUTES]);
